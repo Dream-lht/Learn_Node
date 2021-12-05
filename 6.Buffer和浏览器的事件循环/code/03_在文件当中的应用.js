4440d2a4817104fe685c3fe79174ddfb948875f9
li <you@example.com>
2021-12-05 20:10:58 +0800

    xuexi

--- 6.Buffer和浏览器的事件循环/code/03_在文件当中的应用.js
+++ 6.Buffer和浏览器的事件循环/code/03_在文件当中的应用.js
@@ -5,7 +5,7 @@ fs.readFile('04_test.txt',(err,data) => {
         console.log(err);
         return;
     }
-    //可以通过buffer来解码文件流的二进制
+    //可以通过buffer来解码文件流的二进制zhi
     console.log(data.toString());
 });
 
